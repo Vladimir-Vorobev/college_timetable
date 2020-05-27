faf1f0c471222cea069cd93d3dfe68535ce43091
SerPan23 <igracom@yandex.ru>
2020-05-27 16:46:58 +0300

    Update GroupsTimetable.vue

--- src/components/GroupsTimetable.vue
+++ src/components/GroupsTimetable.vue
@@ -5,6 +5,48 @@
             <option value="/teachers-timetable">Список учителей</option>
             <option value="/classrooms-timetable">Список аудиторий</option>
         </select>
+        <!--Mobile-->
+        <div class="d-block d-sm-block d-md-block d-lg-none d-xl-none list">
+                <div class="grade row">
+                    <span class="num_grade col-12">1 курс</span>
+                    <div class="name_group col-12">t1</div>
+                    <div class="name_group col-12">t1</div>
+                </div>
+                <div class="grade row">
+                    <span class="num_grade col-12">2 курс</span>
+                    <div class="name_group col-12">t1</div>
+                </div>
+                <div class="grade row">
+                    <span class="num_grade col-12">3 курс</span>
+                    <div class="name_group col-12">t1</div>
+                </div>
+                <div class="grade row">
+                    <span class="num_grade col-12">4 курс</span>
+                    <div class="name_group col-12">t1</div>
+                </div>
+        </div>
+        <!-- PC -->
+        <div class="d-none d-sm-none d-md-none d-lg-block d-xl-block list">
+            <div class="row">
+                <div class="grade col-3">
+                    <span class="num_grade">1 курс</span>
+                    <div class="name_group">t1</div>
+                    <div class="name_group">t1</div>
+                </div>
+                <div class="grade col-3">
+                    <span class="num_grade">2 курс</span>
+                    <div class="name_group">t1</div>
+                </div>
+                <div class="grade col-3">
+                    <span class="num_grade">3 курс</span>
+                    <div class="name_group">t1</div>
+                </div>
+                <div class="grade col-3">
+                    <span class="num_grade">4 курс</span>
+                    <div class="name_group">t1</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,5 +62,33 @@ export default {
 .main{
     padding-top: 110px !important;
     min-height: 100vh;
+}
+@media (max-width: 992px) {  
+    .grade{
+        margin-bottom: 1em;
+        margin-left: 0.1em;
+        margin-right: 0.1em;
+    }
+    .num_grade{
+        font-weight: 600;
+        padding-bottom: 0.3em;
+    }
+}
+@media (min-width: 993px) {  
+    .num_grade{
+        font-weight: 600;
+    }
+}
+
+.list{
+    margin-top: 1em;
+    font-size: 1.3em;
+}
+.grade{
+    text-align: center;
+}
+.name_group{
+    border: 1.5px solid #555;
+    margin-bottom: 5px;
 }
 </style>
