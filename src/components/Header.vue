9a456e113f025e288021ff9181d5f60ee126df07
Vladimir <vovav2005@yandex.ru>
2020-05-28 12:27:51 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div id="app">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <router-link to="/" class="router-link">
-            <a class="navbar-brand" ref="home">Главная</a>
+            <a class="navbar-brand">Главная</a>
         </router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -11,13 +11,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <router-link to="/groups-list" class="router-link">
-                  <a class="nav-link" ref="allEvents">Расписание</a>
+              <router-link to="/groups-timetable" class="router-link">
+                  <a class="nav-link">Расписание</a>
               </router-link>
             </li>
             <li class="nav-item">
               <router-link to="/upload-file" class="router-link">
-                  <a class="nav-link" ref="recommendedEvents">Добавить файл</a>
+                  <a class="nav-link">Добавить файл</a>
               </router-link>
             </li>
           </ul>
