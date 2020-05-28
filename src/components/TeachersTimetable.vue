ef5c79a98c173a45b2f5f9809ef3d5bbe68cff63
Vladimir <vovav2005@yandex.ru>
2020-05-28 10:19:25 +0300

    Vova's changes

---
+++ src/components/TeachersTimetable.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main container">
+        <select class="custom-select custom-select-sm mb-3 events" onchange="location.href=this.value">
+            <option value="/groups-timetable" selected>Расписание групп</option>
+            <option value="/teachers-timetable">Расписание учителей</option>
+            <option value="/classrooms-timetable">Расписание аудиторий</option>
+        </select>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'TeachersTimetable',
+    
+}
+</script>
+
+<style scoped>
+.main{
+    padding-top: 110px !important;
+}
+</style>
